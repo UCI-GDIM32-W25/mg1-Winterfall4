e1f2f2a28ca570273df0a8813f833f173eb59398
Winterfal4444 <ugianine143@gmail.com>
2026-01-10 20:20:30 -0800

    Game UI is working

--- Assets/Scripts/PlantCountUI.cs
+++ Assets/Scripts/PlantCountUI.cs
@@ -6,20 +6,40 @@ public class PlantCountUI : MonoBehaviour
     [SerializeField] private TMP_Text _plantedText;
     [SerializeField] private TMP_Text _remainingText;
 
-    public void UpdateSeeds (int seedsLeft, int seedsPlanted)
+    private int seedsLeft;
+    private int seedsPlanted;
+
+    private void Start()
     {
         seedsLeft = 5;
-        seedsPlanted = 5;
+        seedsPlanted = 0;
+    }
+
+    private void Update()
+    {
+        _plantedText.text = seedsPlanted.ToString();
+        _remainingText.text = seedsLeft.ToString();
+        if (Input.GetKeyDown(KeyCode.Space) && seedsLeft > 0)
+        {
+            seedsLeft--;
+            seedsPlanted++;
+            Debug.Log("Space Bar 2");
+        }
+    }
+    
+
+    /* public void UpdateSeeds (int seedsLeft, int seedsPlanted)
+    {
+        seedsLeft = 5;
+        seedsPlanted = 0;
         if (Input.GetKey(KeyCode.Space))
         {
+            seedsLeft--;
+            seedsPlanted++;
             _plantedText.text = seedsLeft.ToString();
             Debug.Log("Space Bar 2");
         }
    
-    }
-    private void Update()
-    {
-    
-    }
+    }*/
 
 }
